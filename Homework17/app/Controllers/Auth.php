682d3a2c666e28c03c4e8ca1c11c4f98bdb7b015
maxvasylchenko <113374488+maxvasylchenko@users.noreply.github.com>
2022-11-02 20:29:49 +0200

    Add files via upload

--- Homework17/app/Controllers/Auth.php
+++ Homework17/app/Controllers/Auth.php
@@ -3,26 +3,36 @@
 namespace App\Controllers;
 use App\Models\User;
 use Core\View;
-class Auth
+class Auth implements ControllerInterface1
 {
-public function login ()
+    public function index ()
+    {
+        $obj = new User;
+        $path = 'viewUsers';
+        $date['people'] = $obj->getAll();
+        View::generate($path, $date);
+    }
+    public function login ()
 {
     $data = array_filter($_POST);
-    var_dump ($data);
     if (!empty($data)){
         $obj = new User();
         $result = $obj->auth($data['email'], $data['password']);
         var_dump($result);
     }
-    View::generate('auth/login', $data);
+    //View::generate('auth/login', $data);
+    $path = 'auth' . DIRECTORY_SEPARATOR . 'insertLogin';
+    View::generate($path);
 }
-public function register()
+    public function register()
 {
     $data = array_filter($_POST);
     if (!empty($data)){
         $obj = new User();
         $obj->createUser($data);
     }
-    View::generate('auth/register', $data);
+    //View::generate('auth/register', $data);
+    $path = 'auth' . DIRECTORY_SEPARATOR . 'insertRegister';
+    View::generate($path);
 }
 }
